d964077bd39b5a180deb5762c8f413fff94ddf3a
Vladyeslav Ukhach <vladyslavukhach@gmail.com>
2026-04-09 19:14:41 +0200

    ball movement

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,17 +8,52 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace breakout
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// private DispatcherTimer gameTimer;
     public partial class MainWindow : Window
     {
+        private DispatcherTimer gameTimer;
+        private Ball ball = new Ball(100,100);
+        private Ellipse ballShape;
         public MainWindow()
         {
             InitializeComponent();
+
+            gameTimer = new DispatcherTimer();
+            gameTimer.Interval = TimeSpan.FromMilliseconds(16);
+            gameTimer.Tick += GameLoop;
+            gameTimer.Start();
+        
+        }
+
+        private void GameLoop(object sender, EventArgs e)
+        {
+            ball.Move();
+            DrawBall();
+        }
+
+        private void DrawBall()
+        {
+            if (ballShape == null)
+            {
+                ballShape = new Ellipse
+                {
+                    Width = ball.Size,
+                    Height = ball.Size,
+                    Fill = Brushes.White
+                };
+
+                GameCanvas.Children.Add(ballShape);
+            }
+
+            Canvas.SetLeft(ballShape, ball.X);
+            Canvas.SetTop(ballShape, ball.Y);
         }
     }
 }
